2ce1e75dbf7883e899ccad1e1f74058d7077f261
Marnie <marnieduarte@yahoo.com>
2014-06-15 09:33:38 -0700

    Create predictOneVsAll.m

---
+++ predictOneVsAll.m
@@ -0,0 +1,34 @@
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+
+A= X *all_theta';
+   %for s = 1:num_labels
+[s,p]= max(A,[],2);
+   %endfor
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
